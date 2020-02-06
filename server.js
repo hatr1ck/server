00b7a39d71c9de7b965592cf9c9fd43c23cdf707
hatr1ck <38593812+hatr1ck@users.noreply.github.com>
2020-02-06 21:48:11 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,10 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const express = require('express');
-const cookieSession = require('cookie-session');
-const bodyParser = require('body-parser');
-const path = require('path');
+var express = require('express');
+var app = express();
 
-const app = express()
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
 
-mongoose.connect('mongodb://ImIn:1k2k3k4k@ds131753.mlab.com:31753/mydb',{ useNewUrlParser: true, useUnifiedTopology: true }).then((data)=>{
-    console.log('connected to mLab')
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    next();
-});
-app.get('/testing', (req,res)=>{
-    res.send('hello')
-})
-app.use(bodyParser.json())
-app.listen(4000, ()=>{
-    console.log('server is up on 4000')
-})
